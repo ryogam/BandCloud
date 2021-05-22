2c84c94f3753e3ab708549b31ce62bb1cbe2ac11
ryogam <garyo08200820@gmail.com>
2021-05-22 23:31:36 +0900

    update pages

--- src/views/pages/App.tsx
+++ src/views/pages/App.tsx
@@ -1,25 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "views/components/block/Header";
+import Footer from "views/components/block/Footer";
+import Top from "views/pages/Top/Top";
+import Contact from "views/pages/Contact/Contact";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Top} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- src/views/pages/Top/Top.tsx
+++ src/views/pages/Top/Top.tsx
@@ -1,1 +1,17 @@
-rm 
+import React, { Fragment } from "react";
+import Introduction from "views/components/block/Introduction";
+import ContactFormComponent from "views/components/block/ContactFrom";
+import StudioList from "views/components/block/StudioList";
+import SideBar from "views/components/block/SideBar";
+
+function Top() {
+  return (
+    <React.Fragment>
+      <Introduction />
+      {/* <SideBar /> */}
+      {/* <StudioList /> */}
+    </React.Fragment>
+  );
+}
+
+export default Top;
