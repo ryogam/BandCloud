ea254a6d8519ee676b4daece746e648459677641
TeppeiSudo <sudo20.t.ab@gmail.com>
2021-05-25 16:36:21 +0900

    update scraping codes

--- scraping/studios/studios/main.py
+++ scraping/studios/studios/main.py
@@ -3,7 +3,7 @@ from scrapy.crawler import CrawlerRunner
 from twisted.internet import reactor, defer
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
-from spiders import noah_spider, penta_spider, noah_group_spider
+from spiders import noah_spider, penta_spider, noah_group_spider, sagasuta_spider
 
 
 configure_logging()
@@ -15,6 +15,7 @@ def crawl():
     yield runner.crawl(noah_spider.NoahSpider)
     yield runner.crawl(penta_spider.PentaSpider)
     yield runner.crawl(noah_group_spider.NoahGroupSpider)
+    yield runner.crawl(sagasuta_spider.SagasutaSpider)
     reactor.stop()
 
 crawl()
--- scraping/studios/studios/spiders/penta_spider.py
+++ scraping/studios/studios/spiders/penta_spider.py
@@ -1,6 +1,7 @@
 import scrapy
 import items
 
+
 class PentaSpider(scrapy.Spider):
     name = "penta_spider"
     allowed_domains = [
@@ -10,17 +11,18 @@ class PentaSpider(scrapy.Spider):
         "http://www.studiopenta.net/rehearsal/index.html",
     ]
 
-    def parse(self, response):
-        penta_items = items.StudiosItem()
 
+    def parse(self, response):
         studio_list_raw = response.xpath(".//section").css("ul")[1].css("li")
         for studio_infos in studio_list_raw:
-            studio_name = studio_infos.xpath(".//h2").css("::text").getall()
-            studio_phone = studio_infos.xpath(".//h3").css("::text").getall()
-            studio_address = studio_infos.css("p.address::text").getall()
+            studio_name = studio_infos.xpath(".//h2").css("::text").get()
+            studio_phone = studio_infos.xpath(".//h3").css("::text").get()
+            studio_address = studio_infos.css("p.address::text").get()
+
+            penta_items = items.StudiosItem()
             penta_items["group"] = "penta"
-            studio_name.remove(" ")
             penta_items["name"] = studio_name
             penta_items["phone"] = studio_phone
             penta_items["address"] = studio_address
+            
             yield penta_items
